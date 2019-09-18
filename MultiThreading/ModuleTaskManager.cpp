0f10fe1390630e3c67d5974a396d2272b4bcb64e
Max <marc.garcia.u@gmail.com>
2019-09-18 16:00:27 +0200

    First attempt

--- MultiThreading/ModuleTaskManager.cpp
+++ MultiThreading/ModuleTaskManager.cpp
@@ -7,9 +7,20 @@ void ModuleTaskManager::threadMain()
 	{
 		// TODO 3:
 		// - Wait for new tasks to arrive
-		// - Retrieve a task from scheduledTasks
-		// - Execute it
-		// - Insert it into finishedTasks
+		if (!scheduledTasks.empty())
+		{
+			std::unique_lock<std::mutex> lock(mtx);
+			// - Retrieve a task from scheduledTasks
+			Task* currentTask = scheduledTasks.front();
+			scheduledTasks.pop();
+
+			// - Execute it
+			currentTask->execute();
+			
+			// - Insert it into finishedTasks
+			finishedTasks.push(currentTask);
+		}
+
 	}
 }
 
@@ -17,13 +28,25 @@ bool ModuleTaskManager::init()
 {
 	// TODO 1: Create threads (they have to execute threadMain())
 
+	for (int i = 0; i < MAX_THREADS; i++)
+	{
+		threads[i] = std::thread(&ModuleTaskManager::threadMain,this);
+	}
+
 	return true;
 }
 
 bool ModuleTaskManager::update()
 {
 	// TODO 4: Dispatch all finished tasks to their owner module (use Module::onTaskFinished() callback)
+	while (!finishedTasks.empty())
+	{
+		Task* finishedTask = finishedTasks.front();
+		finishedTasks.pop();
 
+		finishedTask->owner->onTaskFinished(finishedTask);
+	}
+	
 	return true;
 }
 
@@ -39,4 +62,9 @@ void ModuleTaskManager::scheduleTask(Task *task, Module *owner)
 	task->owner = owner;
 
 	// TODO 2: Insert the task into scheduledTasks so it is executed by some thread
+	{
+		std::unique_lock<std::mutex> lock(mtx);
+		scheduledTasks.push(task);
+		myevent.notify_one();
+	}
 }
